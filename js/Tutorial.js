b1f66301705eda61e15abed80e0b09169cead0e6
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2023-05-23 06:55:13 -0300

    implementação data local.json

--- js/Tutorial.js
+++ js/Tutorial.js
@@ -1,7 +1,7 @@
 var Falas = [
 
     ["Bem vindo ao Math Quiz, aqui faremos perguntas basicas de matematica", "img/Alpha/Explicação.png"],
-    ["e espero que vocÊ se saia bem kk", "img/Alpha/Explicação.png"],
+    ["e espero que você se saia bem kk", "img/Alpha/Explicação.png"],
     ["Este jogo é muit facil de jogar, apenas clique nas opções a baixo", "img/Alpha/Atenção.png"],
     ["e a arraste até o quadrado vasio acima", "img/Alpha/Atenção.png"],
     ["mais cuidado caso erre perdera um lapis (suas vidas)", "img/Alpha/Atenção.png"],
@@ -19,20 +19,23 @@ let Text_box = document.getElementById("Text_box")
 var index = 0
 var fim = true
 
+//import { writeFile } from 'fs'
+ReadData()
 
-if(Tutorial){
+if (Tutorial) {
     Img.src = Falas[index][1]
     Text_box.innerHTML = Falas[index][0]
 }
 
 setInterval(ProximaFala, 3000)
 
-function ProximaFala(){
-    if(fim){
-        if(index >= 6){
+function ProximaFala() {
+    if (fim) {
+        if (index >= 6) {
             Tutorial.remove();
             IniciarJogo()
             fim = false
+            //WriteData()
             return
         }
 
@@ -40,6 +43,27 @@ function ProximaFala(){
         Img.src = Falas[index][1]
         Text_box.innerHTML = Falas[index][0]
 
-        
+
     }
+}
+/*
+function WriteData() {
+    var json = JSON.stringify(fim);
+    writeFile('../data/local.json', json, 'utf8', callback);
+}*/
+
+function ReadData() {
+    fetch('../data/local.json')
+        .then((response) => response.json())
+        .then((json) => {
+
+            if (!json) {
+                Tutorial.remove();
+                IniciarJogo()
+                fim = false
+                //WriteData()
+                return
+            }
+
+        });
 }
